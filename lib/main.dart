b242c979777cb4467c0d1878fb56b61c7999896f
RNSpenc <146198863+RNSpenc@users.noreply.github.com>
2025-09-01 18:47:36 -0400

    update for video 18

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,37 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-    home: Home(),
+    home: QuoteList(),
 ));
 
-class Home extends StatelessWidget {
+class QuoteList extends StatefulWidget {
+  const QuoteList({super.key});
+
+  @override
+  State<QuoteList> createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+
+  List<String> quotes = [
+    'Be yourself; everyone else is already taken.',
+    'I have nothing to declare except my genius.',
+    'The truth is rarely pure and never simple.'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: Text('my first app'),
+        title: const Text('Awesome Quotes'),
         centerTitle: true,
-        backgroundColor: Colors.red[600],
+        backgroundColor: Colors.redAccent,
       ),
-      body: Center(
-        child: Icon(
-          Icons.airport_shuttle,
-          color: Colors.lightBlue,
-          size: 50.0,
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-
-        },
-        backgroundColor: Colors.red[600],
-        child: Text('click'),
+      body: Column(
+        children: quotes.map((quote) => Text(quote)).toList(),
       ),
     );
   }
 }
+
